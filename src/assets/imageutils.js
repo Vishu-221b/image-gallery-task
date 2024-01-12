0fb7669e3c3db423e2714e5526b9b3b9cabd2e3a
vishu <vishwajeetsinghchauhan63@gmail.com>
2024-01-13 02:09:35 +0530

    some css changes

--- src/assets/imageutils.js
+++ src/assets/imageutils.js
@@ -15,30 +15,43 @@ import city from './images/city.jpg';
 import everest from './images/everest.jpg';
 import night from './images/night.jpg';
 import painting from './images/painting.jpg';
+import curtain from './images/curtain.jpg';
+import snail from './images/snail.jpg';
+import girl from './images/girl.jpg';
+import dark from './images/dark.jpg';
 
 
 
 
+    const images = [ 
+        
+        { src: laptop, name: 'laptop', id: 5 },
+        { src: moon, name: 'moon', id: 6 },
+        { src: mountains, name: 'mountains', id: 7 },
+        { src: pebbles, name: 'pebbles', id: 8 },
+        { src: pool, name: 'pool', id: 9 },
+        { src: tattoos, name: 'tattoos', id: 10 },
+        { src: underocean, name: 'underocean', id: 11 },
+        { src: camera, name: 'camera', id: 12 },
+        { src: sunset, name: 'sunset', id: 13},
+        { src: city, name: 'city', id: 14},
+        { src: everest, name: 'everest', id: 15},
+        { src: desert, name: 'desert', id: 2 },
+        { src: night, name: 'night', id: 16},
+        { src: painting, name: 'painting', id: 17},
+        
+        { src: dark, name: 'dark', id: 21 }, // Added the corrected image
+        
+       
+        { src: fog, name: 'fog', id: 3 },
+        { src: iceskate, name: 'iceskate', id: 4 },
+        { src: girl, name: 'girl', id: 20 }, 
+       
+        { src: snail, name: 'snail', id: 19},
+        
+        { src: curtain, name: 'curtain', id: 18},
+        { src: car, name: 'car', id: 1 },
+        
+    ];
 
-
-const images = [ 
-    { src: car, name: 'car', id: 1 },
-    { src: desert, name: 'desert', id: 2 },
-    { src: fog, name: 'fog', id: 3 },
-    { src: iceskate, name: 'iceskate', id: 4 },
-    { src: laptop, name: 'laptop', id: 5 },
-    { src: moon, name: 'moon', id: 6 },
-    { src: mountains, name: 'mountains', id: 7 },
-    { src: pebbles, name: 'pebbles', id: 8 },
-    { src: pool, name: 'pool', id: 9 },
-    { src: tattoos, name: 'tattoos', id: 10 },
-    { src: underocean, name: 'underocean', id: 11 },
-    { src: camera, name: 'camera', id: 12 },
-    { src: sunset, name: 'sunset', id: 13},
-    { src: city, name: 'city', id: 14},
-    { src: everest, name: 'everest', id: 15},
-    { src: night, name: 'night', id: 16},
-    { src: painting, name: 'painting', id: 17},
-]
-
-export default images;
+    export default images;
